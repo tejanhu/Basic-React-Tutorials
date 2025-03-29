741bee9521d2c4e5ed1da2e7d8de9e7d0313ca6c
Hussein Tejan <tejanahmedhu@gmail.com>
2025-03-29 15:54:54 +0000

    first commit message

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import ShoppingList from './ShoppingList';
+import Greeter from './Greeter';
+
+// const data = [
+//   {
+//     id: 1, item: 'dates', quantity: 20, completed: false
+//   },
+//   {
+//     id: 2, item: 'cheese', quantity: 4, completed: true
+//   },
+//   {
+//     id: 3, item: 'milk', quantity: 2, completed: false
+//   },
+//   {
+//     id: 4, item: 'butter', quantity: 5, completed: true
+//   }
+// ];
+
+function App() {
+  return (
+
+    <div> <Greeter person="Hernandez"/> </div>
+    // <div>
+    //     <ShoppingList items={data}/>
+    // </div>
+  )
+}
+
+export default App
---
+++ src/Greeter.jsx
@@ -0,0 +1,4 @@
+export default function Greeter({person}){
+
+    return <h1>As Salaam Alaikum {person}</h1>
+}
